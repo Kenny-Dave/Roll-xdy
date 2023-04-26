2dfeeeb0b2c24d20b6e1ae04f9f6d9fc0502ec80
Kenny Dave <david.campbell993@googlemail.com>
2023-04-26 15:08:15 +0100

    Initial code

---
+++ Roll.py
@@ -0,0 +1,34 @@
+#Roll.py
+import random
+
+inputString ="!roll 3d6"
+rollSum=0
+
+def Roll(inputString):
+
+        nums = inputString.lstrip("!roll ")
+        numList=nums.split("d")
+        
+        rollSum=0
+
+        if len(numList) !=2:
+            return
+        else:
+
+            for r in range(1, int(numList[0])+1):
+                roll = random.choice(range(1, int(numList[1])+1))
+                #print("roll: ", str(r), "Outcome: ",roll)
+                rollSum =rollSum+roll
+
+            return rollSum
+
+if inputString.startswith("!") ==False:
+    pass
+elif inputString.startswith("!roll") ==True:
+    rollSum = Roll(inputString)
+
+    if rollSum!=0:
+        print("With \""+inputString+"\" you rolled "+str(rollSum)+".")
+    else:
+        pass #error
+    rollSum=0
